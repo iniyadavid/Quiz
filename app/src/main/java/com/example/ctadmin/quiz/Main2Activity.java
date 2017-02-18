6b6819abbd6b24484e0926fc19fe46baad7c6897
ins <iniya96@gmail.com>
2017-02-18 16:17:09 +0530

    initial2

---
+++ app/src/main/java/com/example/ctadmin/quiz/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.example.ctadmin.quiz;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+/**
+ * Created by ctadmin on 18-02-2017.
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
--- app/src/main/java/com/example/ctadmin/quiz/MainActivity.java
+++ app/src/main/java/com/example/ctadmin/quiz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ctadmin.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button gotoButton = (Button) findViewById(R.id.button);
+
+
+        gotoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent gotoIntent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(gotoIntent);
+            }
+        });
     }
 }
